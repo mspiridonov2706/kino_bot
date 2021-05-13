5692667a32aa2f05219d0f5ca59e3da65ede95d3
Mikhail Spiridonov <Muxa2793@mail.ru>
2021-05-13 20:38:26 +0300

    fix some bug

--- utils.py
+++ utils.py
@@ -6,7 +6,6 @@ def search_kinopoisk(query, n):
     try:
         page = requests.get(f'https://www.googleapis.com/customsearch/v1?key={GOOGLE_API}&cx={GOOGLE_SID}&q={query}')
         page = page.json()
-        print(page['items'][n]['link'])
         link = page['items'][n]['link']
         return link
     except KeyError:
